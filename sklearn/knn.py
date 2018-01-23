4728d4482d533baad983b5164b6c03e4581e6dc4
cdpolymath <cdpolymath@gmail.com>
2018-01-23 10:24:52 +0100

    Compute prediction rate

--- sklearn/knn.py
+++ sklearn/knn.py
@@ -11,10 +11,11 @@ iris_y = iris.target
 # A random permutation, to split the data randomly
 np.random.seed(0)
 indices = np.random.permutation(len(iris_X))
-iris_X_train = iris_X[indices[:-10]]
-iris_y_train = iris_y[indices[:-10]]
-iris_X_test  = iris_X[indices[-10:]]
-iris_y_test  = iris_y[indices[-10:]]
+ntest = int(0.3*len(indices))
+iris_X_train = iris_X[indices[:-ntest]]
+iris_y_train = iris_y[indices[:-ntest]]
+iris_X_test  = iris_X[indices[-ntest:]]
+iris_y_test  = iris_y[indices[-ntest:]]
 
 
 # Create and fit a nearest-neighbor classifier
@@ -22,7 +23,8 @@ knn = KNeighborsClassifier()
 knn.fit(iris_X_train, iris_y_train) 
 iris_y_predicted = knn.predict(iris_X_test)
 
-print(iris_y_test)
-print(iris_y_predicted)
+# Compute prediction rate
+nerrors = abs(sum(iris_y_test-iris_y_predicted))
+prediction_rate = 1-nerrors/float(ntest)
 
-
+print("prediction rate: ", prediction_rate)
